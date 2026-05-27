703d9805c52cb186797d423d364b98ce235de3ad
andrefgmauricio <andrefgmauricio@gmail.com>
2026-05-27 14:58:26 +0100

    feat: integracao com redis cache funcional para leitura em tempo real

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import os
 import pymysql
 from flask import Flask, request, jsonify
+import redis
 
 
 load_dotenv()
@@ -18,6 +19,9 @@ def get_db_connection():
         cursorclass=pymysql.cursors.DictCursor
     )
 
+r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+
+
 @app.route('/', methods=['GET'])
 def home():
     return jsonify({'status': 'online', 'projeto': 'UalSeed Telemetry'}), 200 
@@ -28,6 +32,11 @@ def receber_telemetria():
 
     if not dados or 'piloto_id' not in dados or 'velocidade' not in dados:
         return jsonify({"erro": "Dados incompletos!"}), 400
+    
+    piloto_id = dados['piloto_id']
+    velocidade = dados['velocidade']
+    rpm = dados.get('rpm', 0)
+    combustivel = dados.get('combustivel', 0.0)
     
     try:
         conexao = get_db_connection()
@@ -40,9 +49,35 @@ def receber_telemetria():
         conexao.commit()
         conexao.close()
 
+        chave_redis=f"piloto:{piloto_id}:atual"
+        r.hset(chave_redis, mapping={
+            "velocidade": velocidade,
+            "rpm": rpm,
+            "combustivel": combustivel
+        })
+
+        r.expire(chave_redis, 60)
+
         return jsonify({"mensagem": "Telemetria recebida com sucesso!"}), 201
     except Exception as e:
         return jsonify({"erro": f"Falha na Base de Dados: {str(e)}"}), 500
+
+@app.route('/api/telemetria/<int:piloto_id>', methods=['GET'])
+def obter_telemetria_atual(piloto_id):
+    chave_redis = f"piloto:{piloto_id}:atual"
+
+    dados_cache = r.hgetall(chave_redis)
+
+    if dados_cache:
+        return jsonify({
+            "fonte": "Redis Cache (RAM)",
+            "piloto_id": piloto_id,
+            "dados": dados_cache
+        }), 200
+                   
+
+    return jsonify({"erro": "Dados em tempo real indisponíveis ou expirados."}), 404
+        
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
